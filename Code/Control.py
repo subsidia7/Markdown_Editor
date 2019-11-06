6b16531f0baa4db1d706a2489d29795e27748438
Cry <subsidia.d@gmail.com>
2019-11-06 16:39:06 +0300

    shit

--- Code/Control.py
+++ Code/Control.py
@@ -8,16 +8,12 @@ class Controller:
         self.set_triggers()
 
     def set_triggers(self):
-        #fileWorking
-
         self.VIEW._new_action.triggered.connect(self.new_file)
-        self.VIEW._open_action.triggered.connect(self.open_file)
-
-
+        self.VIEW._save_action.triggered.connect(self.save_file)
+        self.VIEW._save_AS_action.triggered.connect(self.save_AS_file)
         self.VIEW._markdown_action.triggered.connect(self.markdown_show)
         self.VIEW._all_editors_action.triggered.connect(self.all_edits_show)
         self.VIEW._HTML_action.triggered.connect(self.html_show)
-
 
     def new_file(self):
         self.MODEL.append_document("")
@@ -25,15 +21,23 @@ class Controller:
         self.VIEW.change_active_tab(self.MODEL.ACTIVE_TAB)
 
     def open_file(self):
-        file_path = self.VIEW.select_file()
-        if file_path != False:
-            self.open_file_path(file_path)
+        pass
 
     def save_file(self):
-        pass
+        if self.MODEL.FILE_PATH == Constants.EMPTY_PATH:
+            self.save_AS_file()
+        else:
+            content = self.VIEW.get_current_document_content()
+            self.MODEL.write_file_content(self.MODEL.FILE_PATH, content)
 
     def save_AS_file(self):
-        pass
+        content = self.VIEW.get_current_document_content()
+        file_path = self.VIEW.save_file_picker()
+        if file_path != False:
+            self.MODEL.FILE_PATH = file_path
+            self.MODEL.save_document_path(file_path)
+            self.MODEL.write_file_content(self.MODEL.FILE_PATH, content)
+            self.MODEL.add_recent_document(file_path)
 
     def export_HTML(self):
         pass
@@ -61,26 +65,3 @@ class Controller:
         html = self.VIEW.get_active_preview()
         markdown.show()
         html.show()
-
-    def open_file_path(self, file_path):
-        file_content = self.MODEL.get_file_content_utf8(file_path)
-
-        if file_content is False:
-            #self.VIEW.no_file_alert()
-            return False
-
-        doc_ix = self.MODEL.is_document_present(file_path)
-        if doc_ix != -1:
-            self.MODEL.ACTIVE_TAB = doc_ix
-            self.VIEW.change_active_tab(self.MODEL.ACTIVE_TAB)
-        else :
-            self.MODEL.append_document(file_path)
-            self.VIEW.change_active_tab(self.MODEL.ACTIVE_TAB)
-            #self.VIEW.set_document(unicode(file_content, 'utf-8'))
-
-
-
-
-
-
-
